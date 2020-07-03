5b1b88242913c3d51e20d14370dd0d00ee39931f
David Siebo <zentraal@gmail.com>
2020-07-02 23:55:32 -0700

    register models with admin panel

--- okra/admin.py
+++ okra/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Objective
+from .models import KeyResult
 
-# Register your models here.
+
+admin.site.register(Objective)
+admin.site.register(KeyResult)
